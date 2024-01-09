0385c46ba620af4105fbb734121a00ee9f07308f
Thaw Zin Tun <thawzintun98@gmail.com>
2024-01-09 14:28:58 +0630

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 let mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -14,29 +14,35 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
-    done(null /*, data*/);
+    Person.create({
+        name: "Kyaw Gyi",
+        age: 22,
+        favoriteFoods: ["Apple", "Orange", "Grape"],
+    });
+    done(null);
 };
 
-// var arrayOfPeople = [
-//     {
-//         name: "Steve",
-//         age: 30,
-//         favoriteFoods: ["Apple", "Orange", "Grape"],
-//     },
-//     {
-//         name: "Tchalla",
-//         age: 25,
-//         favoriteFoods: ["Watermelon", "Durian", "Pineapple"],
-//     },
-//     {
-//         name: "Peter",
-//         age: 19,
-//         favoriteFoods: ["Avocado", "Banana", "Mango"],
-//     },
-// ];
+var arrayOfPeople = [
+    {
+        name: "Steve",
+        age: 30,
+        favoriteFoods: ["Apple", "Orange", "Grape"],
+    },
+    {
+        name: "Tchalla",
+        age: 25,
+        favoriteFoods: ["Watermelon", "Durian", "Pineapple"],
+    },
+    {
+        name: "Peter",
+        age: 19,
+        favoriteFoods: ["Avocado", "Banana", "Mango"],
+    },
+];
 
 const createManyPeople = (arrayOfPeople, done) => {
-    done(null /*, data*/);
+    Person.create(arrayOfPeople);
+    done(null);
 };
 
 const findPeopleByName = (personName, done) => {
